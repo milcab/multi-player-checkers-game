3f9c32d6c5f49aad640fa1b7170efacaa5eb1174
milcab <milcarodriguez23@gmail.com>
2021-11-24 19:42:22 -0500

    add tokens

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -1,4 +1,4 @@
-let board = [
+let rows = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -9,7 +9,63 @@ let board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
-board.forEach((row, rowIndex) => {
-    row.forEach((col, colIndex) => console.log({ rowIndex, colIndex }))
+const boardEl = document.querySelector('.board');
+
+const whiteTokens = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+const redTokens = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+rows.forEach((cols, rowIndex) => {
+    const rowEl = createRow()
+    cols.forEach((col, colIndex) => {
+        const colEl = createCol()
+        const isEven = (rowIndex + colIndex) % 2 === 0
+        const isEmptyRow = rowIndex === 3 || rowIndex === 4
+
+        if (isEven && !isEmptyRow) {
+
+            if (whiteTokens.length) {
+                whiteTokens.pop()
+                const token = createToken()
+
+                token.classList.add('white');
+
+                colEl.appendChild(token)
+            } else if (redTokens.length) {
+                redTokens.pop()
+                const token = createToken()
+
+                token.classList.add('red');
+
+                colEl.appendChild(token)
+            }
+
+        }
+
+        rowEl.appendChild(colEl)
+    })
+
+
+    boardEl.appendChild(rowEl)
 })
 
+function createToken() {
+    const token = document.createElement('div')
+    token.classList.add('token')
+
+    return token
+}
+
+
+function createRow() {
+    const row = document.createElement('div')
+    row.classList.add('row')
+
+    return row
+}
+
+function createCol() {
+    const col = document.createElement('div')
+    col.classList.add('column')
+
+    return col
+}
