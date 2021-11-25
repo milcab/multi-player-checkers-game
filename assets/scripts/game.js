554c160bbd503d83e19c3e3e8a38843d9ea7d8d3
milcab <milcarodriguez23@gmail.com>
2021-11-24 19:06:19 -0500

    add board and token styles

---
+++ assets/scripts/game.js
@@ -0,0 +1,14 @@
+// how can you keep track of the score.
+// how do you determine a winning score.
+// how do you determine a losing score.
+// how do you alert the user wether they lost or won.
+// how do you start the game again(restart).
+// how you can track wins and losses.
+// how do you handle draws
+let score
+let winningScore
+let loosingScore
+let whoWon
+let trakingWhoWins
+let restartGame
+let handleDraws
